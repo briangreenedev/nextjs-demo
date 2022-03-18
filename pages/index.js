8acd267379720d8998eee6c9fbdcab562aa745f5
bgreene <brian.greene@eg-america.com>
2022-03-18 09:01:59 -0600

    change number color to purple

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
             <p>I know you cannot resist</p>
           </a>
         </div>
-        <div style={{fontSize:`${3 * n}px`, fontWeight: '900', color:n === 100 && 'green', height: '350px'}}>
+        <div style={{fontSize:`${3 * n}px`, fontWeight: '900', color:n === 100 && 'green' || 'purple', height: '350px'}}>
           {n}
         </div>
       </main>
